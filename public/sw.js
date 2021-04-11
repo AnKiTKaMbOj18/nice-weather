31563d4ed4b543939e7dd74319c76c5f236444e1
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-04-11 22:47:46 +0200

    WA | added service workers for PWA

---
+++ public/sw.js
@@ -0,0 +1,49 @@
+let CACHE_NAME = 'nyc-weather';
+let urlsToCache = [
+  '/',
+  "/static/css/2.ec02d681.chunk.css",
+  "/static/js/main.0f590be8.chunk.js",
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  let cacheWhitelist = ['nyc-weather'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
